1a76966fd7afd983bb3509ce9998c6789fc3997f
Paul Dima <pauldima95@gmail.com>
2022-08-30 19:47:41 +0200

    replaced popcount with std::popcount

--- include/dice/sparse-map/sparse_hash.hpp
+++ include/dice/sparse-map/sparse_hash.hpp
@@ -38,17 +38,10 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
+#include <bit> // std::popcount
 
 #include "dice/sparse-map/sparse_growth_policy.hpp"
 #include "boost/container/vector.hpp"
-
-#ifdef __INTEL_COMPILER
-#include <immintrin.h>  // For _popcnt32 and _popcnt64
-#endif
-
-#ifdef _MSC_VER
-#include <intrin.h>  // For __cpuid, __popcnt and __popcnt64
-#endif
 
 #ifdef TSL_DEBUG
 #define tsl_sh_assert(expr) assert(expr)
@@ -65,118 +58,6 @@ enum class exception_safety { basic, strong };
 
 enum class sparsity { high, medium, low };
 }  // namespace sh
-
-namespace detail_popcount {
-/**
- * Define the popcount(ll) methods and pick-up the best depending on the
- * compiler.
- */
-
-// From Wikipedia: https://en.wikipedia.org/wiki/Hamming_weight
-inline int fallback_popcountll(unsigned long long int x) {
-  static_assert(
-      sizeof(unsigned long long int) == sizeof(std::uint64_t),
-      "sizeof(unsigned long long int) must be equal to sizeof(std::uint64_t). "
-      "Open a feature request if you need support for a platform where it "
-      "isn't the case.");
-
-  const std::uint64_t m1 = 0x5555555555555555ull;
-  const std::uint64_t m2 = 0x3333333333333333ull;
-  const std::uint64_t m4 = 0x0f0f0f0f0f0f0f0full;
-  const std::uint64_t h01 = 0x0101010101010101ull;
-
-  x -= (x >> 1ull) & m1;
-  x = (x & m2) + ((x >> 2ull) & m2);
-  x = (x + (x >> 4ull)) & m4;
-  return static_cast<int>((x * h01) >> (64ull - 8ull));
-}
-
-inline int fallback_popcount(unsigned int x) {
-  static_assert(sizeof(unsigned int) == sizeof(std::uint32_t) ||
-                    sizeof(unsigned int) == sizeof(std::uint64_t),
-                "sizeof(unsigned int) must be equal to sizeof(std::uint32_t) "
-                "or sizeof(std::uint64_t). "
-                "Open a feature request if you need support for a platform "
-                "where it isn't the case.");
-
-  if (sizeof(unsigned int) == sizeof(std::uint32_t)) {
-    const std::uint32_t m1 = 0x55555555;
-    const std::uint32_t m2 = 0x33333333;
-    const std::uint32_t m4 = 0x0f0f0f0f;
-    const std::uint32_t h01 = 0x01010101;
-
-    x -= (x >> 1) & m1;
-    x = (x & m2) + ((x >> 2) & m2);
-    x = (x + (x >> 4)) & m4;
-    return static_cast<int>((x * h01) >> (32 - 8));
-  } else {
-    return fallback_popcountll(x);
-  }
-}
-
-#if defined(__clang__) || defined(__GNUC__)
-inline int popcountll(unsigned long long int value) {
-  return __builtin_popcountll(value);
-}
-
-inline int popcount(unsigned int value) { return __builtin_popcount(value); }
-
-#elif defined(_MSC_VER)
-/**
- * We need to check for popcount support at runtime on Windows with __cpuid
- * See https://msdn.microsoft.com/en-us/library/bb385231.aspx
- */
-inline bool has_popcount_support() {
-  int cpu_infos[4];
-  __cpuid(cpu_infos, 1);
-  return (cpu_infos[2] & (1 << 23)) != 0;
-}
-
-inline int popcountll(unsigned long long int value) {
-#ifdef _WIN64
-  static_assert(
-      sizeof(unsigned long long int) == sizeof(std::int64_t),
-      "sizeof(unsigned long long int) must be equal to sizeof(std::int64_t). ");
-
-  static const bool has_popcount = has_popcount_support();
-  return has_popcount
-             ? static_cast<int>(__popcnt64(static_cast<std::int64_t>(value)))
-             : fallback_popcountll(value);
-#else
-  return fallback_popcountll(value);
-#endif
-}
-
-inline int popcount(unsigned int value) {
-  static_assert(sizeof(unsigned int) == sizeof(std::int32_t),
-                "sizeof(unsigned int) must be equal to sizeof(std::int32_t). ");
-
-  static const bool has_popcount = has_popcount_support();
-  return has_popcount
-             ? static_cast<int>(__popcnt(static_cast<std::int32_t>(value)))
-             : fallback_popcount(value);
-}
-
-#elif defined(__INTEL_COMPILER)
-inline int popcountll(unsigned long long int value) {
-  static_assert(sizeof(unsigned long long int) == sizeof(__int64), "");
-  return _popcnt64(static_cast<__int64>(value));
-}
-
-inline int popcount(unsigned int value) {
-  return _popcnt32(static_cast<int>(value));
-}
-
-#else
-inline int popcountll(unsigned long long int x) {
-  return fallback_popcountll(x);
-}
-
-inline int popcount(unsigned int x) { return fallback_popcount(x); }
-
-#endif
-}  // namespace detail_popcount
-
 
 /* Replacement for const_cast in sparse_array.
  * Can be overloaded for specific fancy pointers
@@ -770,18 +651,9 @@ class sparse_array {
     alloc.deallocate(values, capacity_values);
   }
 
-  static size_type popcount(bitmap_type val) noexcept {
-    if (sizeof(bitmap_type) <= sizeof(unsigned int)) {
-      return static_cast<size_type>(
-          dice::sparse_map::detail_popcount::popcount(static_cast<unsigned int>(val)));
-    } else {
-      return static_cast<size_type>(dice::sparse_map::detail_popcount::popcountll(val));
-    }
-  }
-
   size_type index_to_offset(size_type index) const noexcept {
     tsl_sh_assert(index < BITMAP_NB_BITS);
-    return popcount(m_bitmap_vals &
+    return std::popcount(m_bitmap_vals &
                     ((bitmap_type(1) << index) - bitmap_type(1)));
   }
 
