f66676204a1f067be6eaa0fc2ccbb146ceecc55a
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2025-09-02 14:12:08 +0200

    Feature/merge from upstream (#30)
    * remove old clang-format
    
    * reformat everything
    
    * Avoid iterator invalidation without effective insert
    
    * Fix compilation warning on temporary variable
    
    * fix ci, bump pobr
    
    * bump linux/compiler versions
    
    * more versions
    
    * doc update
    
    * revert pobr

--- include/dice/sparse-map/boost_offset_pointer.hpp
+++ include/dice/sparse-map/boost_offset_pointer.hpp
@@ -3,25 +3,25 @@
 
 #if __has_include(<boost/interprocess/offset_ptr.hpp>)
 
-#include "dice/sparse-map/sparse_hash.hpp" //needed, so the basic template is already included
+#include "dice/sparse-map/sparse_hash.hpp"  //needed, so the basic template is already included
 #include <boost/interprocess/offset_ptr.hpp>
 
 namespace dice::sparse_map {
-/* Template specialisation for a "const_cast" of a boost offset_ptr.
- * @tparam PT PointedType
- * @tparam DT DifferenceType
- * @tparam OT OffsetType
- * @tparam OA OffsetAlignment
- */
-template <typename PT, typename DT, typename OT, std::size_t OA>
-struct Remove_Const<boost::interprocess::offset_ptr<PT, DT, OT, OA>> {
-  template <typename T>
-  static boost::interprocess::offset_ptr<PT, DT, OT, OA>
-  remove(T const &const_iter) {
-    return boost::interprocess::const_pointer_cast<PT, DT, OT, OA>(const_iter);
-  }
-};
-} // namespace dice
+    /* Template specialisation for a "const_cast" of a boost offset_ptr.
+     * @tparam PT PointedType
+     * @tparam DT DifferenceType
+     * @tparam OT OffsetType
+     * @tparam OA OffsetAlignment
+     */
+    template<typename PT, typename DT, typename OT, std::size_t OA>
+    struct Remove_Const<boost::interprocess::offset_ptr<PT, DT, OT, OA>> {
+        template<typename T>
+        static boost::interprocess::offset_ptr<PT, DT, OT, OA>
+        remove(T const &const_iter) {
+            return boost::interprocess::const_pointer_cast<PT, DT, OT, OA>(const_iter);
+        }
+    };
+}  // namespace dice::sparse_map
 
-#endif // __has_include(<boost/interprocess/offset_ptr.hpp>)
-#endif // DICE_SPARSE_MAP_BOOST_OFFSET_POINTER_HPP
+#endif  // __has_include(<boost/interprocess/offset_ptr.hpp>)
+#endif  // DICE_SPARSE_MAP_BOOST_OFFSET_POINTER_HPP
--- tests/fancy_pointer/sparse_array_tests.cpp
+++ tests/fancy_pointer/sparse_array_tests.cpp
@@ -2,28 +2,28 @@
  * @brief Checks for fancy pointer support in the sparse_array implementation.
  */
 
+#include "CustomAllocator.hpp"
 #include <boost/test/unit_test.hpp>
 #include <dice/sparse-map/sparse_hash.hpp>
-#include "CustomAllocator.hpp"
 
 // Globals
-constexpr auto MAX_INDEX = 32; //BITMAP_NB_BITS
+constexpr auto MAX_INDEX = 32;  // BITMAP_NB_BITS
 
 /* Tests are formulated via templates to reduce code duplication.
  * The template parameter contains the Allocator type and the shorthand "Array" for the sparse_array
  * (with all template parameter already inserted).
  */
 
-template <typename T>
+template<typename T>
 void compilation() {
     typename T::Array test;
 }
 
-template <typename T>
+template<typename T>
 void construction() {
     typename T::Allocator a;
     typename T::Array test(MAX_INDEX, a);
-    test.clear(a); //needed because destructor asserts
+    test.clear(a);  // needed because destructor asserts
 }
 
 namespace details {
@@ -44,22 +44,22 @@ namespace details {
         }
         return check;
     }
-}
+}  // namespace details
 
-template <typename T>
+template<typename T>
 void set() {
     typename T::Allocator a;
     auto test = details::generate_test_array<T>(a);
     auto check = details::generate_check_for_test_array<T>();
     //'set' did not create the correct order of items
     BOOST_REQUIRE(std::equal(test.begin(), test.end(), check.begin()));
-    test.clear(a); //needed because destructor asserts
+    test.clear(a);  // needed because destructor asserts
 }
 
-template <typename T>
+template<typename T>
 void copy_construction() {
     typename T::Allocator a;
-    //needs to be its own line, otherwise the move-construction would take place
+    // needs to be its own line, otherwise the move-construction would take place
     auto test = details::generate_test_array<T>(a);
     typename T::Array copy(test, a);
     auto check = details::generate_check_for_test_array<T>();
@@ -69,10 +69,10 @@ void copy_construction() {
     copy.clear(a);
 }
 
-template <typename T>
+template<typename T>
 void move_construction() {
     typename T::Allocator a;
-    //two lines needed. Otherwise move/copy elision
+    // two lines needed. Otherwise move/copy elision
     auto moved_from = details::generate_test_array<T>(a);
     typename T::Array moved_to(std::move(moved_from));
     auto check = details::generate_check_for_test_array<T>();
@@ -81,12 +81,12 @@ void move_construction() {
     moved_to.clear(a);
 }
 
-template <typename T>
+template<typename T>
 void const_iterator() {
     typename T::Allocator a;
     auto test = details::generate_test_array<T>(a);
     auto const_iter = test.cbegin();
-    //const iterator has the wrong type
+    // const iterator has the wrong type
     BOOST_REQUIRE((std::is_same<decltype(const_iter), typename T::Const_Iterator>::value));
     test.clear(a);
 }
@@ -95,20 +95,19 @@ void const_iterator() {
 /*
  * This are the types you can give the tests as template parameters.
  */
-template <typename T, dice::sparse_map::sh::sparsity Sparsity = dice::sparse_map::sh::sparsity::medium>
+template<typename T, dice::sparse_map::sh::sparsity Sparsity = dice::sparse_map::sh::sparsity::medium>
 struct STD {
     using Allocator = std::allocator<T>;
     using Array = dice::sparse_map::detail_sparse_hash::sparse_array<T, std::allocator<T>, Sparsity>;
-    using Const_Iterator = T const*;
+    using Const_Iterator = T const *;
 };
 
 template<typename T, dice::sparse_map::sh::sparsity Sparsity = dice::sparse_map::sh::sparsity::medium>
 struct CUSTOM {
     using Allocator = OffsetAllocator<T>;
     using Array = dice::sparse_map::detail_sparse_hash::sparse_array<T, OffsetAllocator<T>, Sparsity>;
-    using Const_Iterator = boost::interprocess::offset_ptr<const T>;
+    using Const_Iterator = boost::interprocess::offset_ptr<T const>;
 };
-
 
 
 /* The instantiation of the tests.
@@ -117,19 +116,43 @@ struct CUSTOM {
 BOOST_AUTO_TEST_SUITE(fancy_pointers)
 BOOST_AUTO_TEST_SUITE(sparse_array_tests)
 
-BOOST_AUTO_TEST_CASE(std_alloc_compile) {compilation<STD<int>>();}
-BOOST_AUTO_TEST_CASE(std_alloc_construction) {construction<STD<int>>();}
-BOOST_AUTO_TEST_CASE(std_alloc_set) {set<STD<int>>();}
-BOOST_AUTO_TEST_CASE(std_alloc_copy_construction) {copy_construction<STD<int>>();}
-BOOST_AUTO_TEST_CASE(std_alloc_move_construction) {move_construction<STD<int>>();}
-BOOST_AUTO_TEST_CASE(std_const_iterator) {const_iterator<STD<int>>();}
+BOOST_AUTO_TEST_CASE(std_alloc_compile) {
+    compilation<STD<int>>();
+}
+BOOST_AUTO_TEST_CASE(std_alloc_construction) {
+    construction<STD<int>>();
+}
+BOOST_AUTO_TEST_CASE(std_alloc_set) {
+    set<STD<int>>();
+}
+BOOST_AUTO_TEST_CASE(std_alloc_copy_construction) {
+    copy_construction<STD<int>>();
+}
+BOOST_AUTO_TEST_CASE(std_alloc_move_construction) {
+    move_construction<STD<int>>();
+}
+BOOST_AUTO_TEST_CASE(std_const_iterator) {
+    const_iterator<STD<int>>();
+}
 
-BOOST_AUTO_TEST_CASE(custom_alloc_compile) {compilation<CUSTOM<int>>();}
-BOOST_AUTO_TEST_CASE(custom_alloc_construction) {construction<CUSTOM<int>>();}
-BOOST_AUTO_TEST_CASE(custom_alloc_set) {set<CUSTOM<int>>();}
-BOOST_AUTO_TEST_CASE(custom_alloc_copy_construction) {copy_construction<CUSTOM<int>>();}
-BOOST_AUTO_TEST_CASE(custom_alloc_move_construction) {move_construction<CUSTOM<int>>();}
-BOOST_AUTO_TEST_CASE(custom_const_iterator) {const_iterator<CUSTOM<int>>();}
+BOOST_AUTO_TEST_CASE(custom_alloc_compile) {
+    compilation<CUSTOM<int>>();
+}
+BOOST_AUTO_TEST_CASE(custom_alloc_construction) {
+    construction<CUSTOM<int>>();
+}
+BOOST_AUTO_TEST_CASE(custom_alloc_set) {
+    set<CUSTOM<int>>();
+}
+BOOST_AUTO_TEST_CASE(custom_alloc_copy_construction) {
+    copy_construction<CUSTOM<int>>();
+}
+BOOST_AUTO_TEST_CASE(custom_alloc_move_construction) {
+    move_construction<CUSTOM<int>>();
+}
+BOOST_AUTO_TEST_CASE(custom_const_iterator) {
+    const_iterator<CUSTOM<int>>();
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
